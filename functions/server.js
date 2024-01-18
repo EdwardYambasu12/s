d2b75a7b144f4ac2533dce4e97f38b9f3e902eb4
EdwardYambasu12 <yambasuedward4@gmail.com>
2024-01-18 14:39:40 +0000

    anothe4 commit

--- functions/server.js
+++ functions/server.js
@@ -1,14 +1,13 @@
 const express = require("express")
 const request = require("request")
-const functions = require("firebase-functions")
-const firebase_admin = require("firebase-admin ")
+
 const cors = require("cors")
 const data = require("./data")
 const app = express()
 app.use(cors())
 app.use(data)
 
-firebase_admin.initializeApp(functions.config().firebase)
+
 
 
 
@@ -17,4 +16,3 @@ app.listen(6100, ()=>{
     console.log("server is running on port 6100")
 })
 
-exports.app = functions.https.onRequest(app);
